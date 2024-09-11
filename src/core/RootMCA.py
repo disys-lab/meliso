d7d778459d5aec090b1e573e6de33bea51aae623
Quang-Nguyen Vo-Huynh <quangnguyenvohuynh@gmail.com>
2024-09-11 13:50:22 -0500

    newest update

--- src/core/RootMCA.py
+++ src/core/RootMCA.py
@@ -77,7 +77,6 @@ class RootMCA(BaseMCA):
     def readMatrix(self,filename):
         if not os.path.isfile(filename):
             raise Exception("MatrixFileNotFoundError:The file %s does not exist or is invalid".format(filename))
-        #0.5*np.eye(66,) #
         mat = mmread(filename)
 
         if not isinstance(mat, np.ndarray):
