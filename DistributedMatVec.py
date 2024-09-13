2ffad1e2e32c39185eb16fb20c7d17bfd464196d
Quang-Nguyen Vo-Huynh <quangnguyenvohuynh@gmail.com>
2024-09-13 10:29:09 -0500

    newest update

--- DistributedMatVec.py
+++ DistributedMatVec.py
@@ -1,13 +1,17 @@
-import meliso
+import meliso, time
 import numpy as np
 from solver.matvec.MatVecSolver import MatVecSolver
 
 correction=False
 
-mv = MatVecSolver()
+start_time = time.time()
 
+mv = MatVecSolver()
 mv.matVec(correction=correction)
 mv.finalize()
 mv.parallelizedBenchmarkMatVec(0,0,correction=correction)
-mv.acquireMCAStats()
+# mv.acquireMCAStats()
 mv.finalize()
+
+end_time = time.time()
+print(f"Elapsed time for the entire VMM operation: {end_time - start_time}")
