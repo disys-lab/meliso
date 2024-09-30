21bdb14e8d8b81626bf508e57e2f14740be29586
Quang-Nguyen Vo-Huynh <quangnguyenvohuynh@gmail.com>
2024-09-30 15:19:02 -0500

    Update DistributedMatVec.py

--- DistributedMatVec.py
+++ DistributedMatVec.py
@@ -4,21 +4,21 @@ from solver.matvec.MatVecSolver import MatVecSolver
 
 start_time = time.time()
 
-correction=False
+correction = False
 mv = MatVecSolver()
 mv.matVec(correction=correction)
 mv.finalize()
 mv.acquireMCAStats()
 
-if int(os.environ["CENTRALIZED"]) == 1:
-    mv.centralizedBenchmarkMatVec()
-    mv.finalize()
-    end_time = time.time()
-    print(f"Elapsed time for the entire VMM operation: {end_time - start_time}")
-    exit()
-else:
-    mv.parallelizedBenchmarkMatVec(0,0,correction=correction)
-    mv.finalize()
-    end_time = time.time()
-    print(f"Elapsed time for the entire VMM operation: {end_time - start_time}")
-    exit()
+# if int(os.environ["CENTRALIZED"]) == 1:
+#     mv.centralizedBenchmarkMatVec()
+#     mv.finalize()
+#     end_time = time.time()
+#     print(f"Elapsed time for the entire VMM operation: {end_time - start_time}")
+#     exit()
+# else:
+
+mv.parallelizedBenchmarkMatVec(0,0,correction=correction)
+mv.finalize()
+end_time = time.time()
+print(f"Elapsed time for the entire VMM operation: {end_time - start_time}")
