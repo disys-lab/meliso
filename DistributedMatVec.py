721bc0d9dd07ed767b5548cee6ad2aa1f5ea9b47
Quang-Nguyen Vo-Huynh <quangnguyenvohuynh@gmail.com>
2024-09-30 15:10:06 -0500

    Update DistributedMatVec.py

--- DistributedMatVec.py
+++ DistributedMatVec.py
@@ -12,8 +12,9 @@ mv.acquireMCAStats()
 
 if int(os.environ["CENTRALIZED"]) == 1:
     mv.centralizedBenchmarkMatVec()
+    mv.finalize()
 else:
     mv.parallelizedBenchmarkMatVec(0,0,correction=correction)
-mv.finalize()
+    mv.finalize()
 end_time = time.time()
 print(f"Elapsed time for the entire VMM operation: {end_time - start_time}")
