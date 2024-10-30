09fe1e6c105ae9d5ce259a069f8c73a56ca6e9e1
Quang-Nguyen Vo-Huynh <quangnguyenvohuynh@gmail.com>
2024-10-30 15:35:08 -0500

    Update run_virtual_weakScaling.sh

--- run_virtual_weakScaling.sh
+++ run_virtual_weakScaling.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
-#SBATCH -p long
+#SBATCH -p cascadelake
 #SBATCH -t 12:00:00
-#SBATCH --nodes=33
-#SBATCH --ntasks-per-node=32
+#SBATCH --ntasks=1030
 #SBATCH --mail-user=lucius.vo@okstate.edu
-#SBATCH --mail-type=end
+#SBATCH --mail-type=END
+
+# Exit immediately if a command exits with a non-zero status
+set -e
 
 # Setup
-source activate mpienv38
+module load anaconda3/2022.10
+module load gcc/7.5.0
+source "$(conda info --base)/etc/profile.d/conda.sh"
+conda activate mpienv38
 export PYTHONPATH=$PYTHONPATH:./build
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:./build/
 
@@ -28,7 +33,7 @@ EXPERIMENTS=("exp1.yaml" "exp2.yaml" "exp3.yaml" "exp4.yaml")
 PROCESSORS=(1025 257 65 17)  # Number of processors for each experiment
 
 # Constant ITER_LIMIT value
-ITER_LIMIT=100
+ITER_LIMIT=10
 
 # Common input vector path
 XVEC_PATH="inputs/vectors/input_x.txt"
@@ -44,7 +49,7 @@ for material in "${!MATERIALS[@]}"; do
     # Run the experiment REPS times with the constant ITER_LIMIT
     for ((i=1; i<=REPS; i++)); do
       echo "Running ${material} with ${EXPERIMENTS[$idx]}, ITER_LIMIT=${ITER_LIMIT}, repetition $i using ${NUM_PROCESSORS} processors"
-      REPORT_PATH="exp_reports/virtualization/weakScaling/${material}/${EXPERIMENTS[$idx]%.yaml}_iter_${ITER_LIMIT}_rep_${i}.txt"
+      REPORT_PATH="reports/virtualization/weakScaling/${material}/${EXPERIMENTS[$idx]%.yaml}_iter_${ITER_LIMIT}_rep_${i}.txt"
       
       # Remove old report file if it exists
       if [ -f "$REPORT_PATH" ]; then
@@ -57,6 +62,5 @@ for material in "${!MATERIALS[@]}"; do
       EXP_CONFIG_FILE=$EXP_CONFIG_FILE REPORT_PATH=$REPORT_PATH \
       mpiexec -n $NUM_PROCESSORS python3 DistributedMatVec.py
     done
-
   done
 done
