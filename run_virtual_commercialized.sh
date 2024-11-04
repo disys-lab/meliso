d0a9fb49f690a0aa2d6b3f310099f8a27f6de79e
Quang-Nguyen Vo-Huynh <quangnguyenvohuynh@gmail.com>
2024-11-04 11:34:03 -0600

    update experiments

--- run_virtual_commercialized.sh
+++ run_virtual_commercialized.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 #SBATCH -p cascadelake
 #SBATCH -t 12:00:00
-#SBATCH --nodes=1
-#SBATCH --ntasks=32
-#SBATCH --cpus-per-task=1
+#SBATCH --ntasks=1030
 #SBATCH --mail-user=lucius.vo@okstate.edu
 #SBATCH --mail-type=END
 
@@ -17,15 +15,53 @@ source "$(conda info --base)/etc/profile.d/conda.sh"
 conda activate mpienv38
 
 # Set up environment variables
-export REPORT_PATH="reports/quickstart/exp1.txt"
-export PYTHONPATH="$PYTHONPATH:./build"
-export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:./build/"
-export DT=1
-export OVERRIDE=1
-export ITER_LIMIT=100
-export XVEC_PATH="inputs/vectors/input_x.txt"
-export EXP_CONFIG_FILE="config_files/virtualization/commercialized/exp1.yaml"
-export REPORT_PATH="reports/virtualization/commercialized/exp1.txt"
+export PYTHONPATH=$PYTHONPATH:./build
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:./build/
 
-# Run the parallel job
-mpiexec -n 2 python3 DistributedMatVec.py
+# Number of replications
+REPS=5
+
+# Number of iterations
+ITER_LIMIT=10
+
+# List of materials and corresponding config paths for each experiment
+declare -A MATERIALS
+MATERIALS=( 
+    ["Ag-aSi"]="config_files/virtualization/commercialized/Ag-aSi/"
+    ["AlOx-HfO2"]="config_files/virtualization/commercialized/AlOx-HfO2/"
+    ["EpiRAM"]="config_files/virtualization/commercialized/EpiRAM/"
+    ["TaOx-HfOx"]="config_files/virtualization/commercialized/TaOx-HfOx/"
+)
+
+# List of experiment file names and corresponding number of processors
+EXPERIMENTS=("exp1.yaml" "exp2.yaml" "exp3.yaml" "exp4.yaml" "exp5.yaml" "exp6.yaml")
+PROCESSORS=10
+
+# Common input vector path
+XVEC_PATH="inputs/vectors/input_x.txt"
+
+# Loop over each material
+for material in "${!MATERIALS[@]}"; do
+
+  # Loop over each experiment configuration
+  for idx in "${!EXPERIMENTS[@]}"; do
+    EXP_CONFIG_FILE="${MATERIALS[$material]}${EXPERIMENTS[$idx]}"
+
+    # Run the experiment REPS times with the constant ITER_LIMIT
+    for ((i=1; i<=REPS; i++)); do
+      echo "Running ${material} with ${EXPERIMENTS[$idx]}, ITER_LIMIT=${ITER_LIMIT}, repetition $i using ${NUM_PROCESSORS} processors"
+      REPORT_PATH="reports/virtualization/weakScaling/${material}/${EXPERIMENTS[$idx]%.yaml}_iter_${ITER_LIMIT}_rep_${i}.txt"
+      
+      # Remove old report file if it exists
+      if [ -f "$REPORT_PATH" ]; then
+        echo "Removing old report file: $REPORT_PATH"
+        rm "$REPORT_PATH"
+      fi
+
+      # Run the experiment
+      DT=1 OVERRIDE=1 ITER_LIMIT=$ITER_LIMIT XVEC_PATH=$XVEC_PATH \
+      EXP_CONFIG_FILE=$EXP_CONFIG_FILE REPORT_PATH=$REPORT_PATH \
+      mpiexec -n $NUM_PROCESSORS python3 DistributedMatVec.py
+    done
+  done
+done
